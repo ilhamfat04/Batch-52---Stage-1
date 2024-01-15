d2b2b7ca93075c18a5200f34ee14c42c8da76e3d
Ilham Fathullah <ilhamfathullah04@gmail.com>
2024-01-15 11:31:07 +0700

    day 12 - array of object manipluation

--- index.js
+++ index.js
@@ -7,12 +7,18 @@ app.set('view engine', 'hbs')
 app.set('views', 'src/views')
 
 app.use('/assets', express.static('src/assets'))
+app.use(express.urlencoded({ extended: false })) // body parser
 
 app.get('/', home)
 app.get('/contact', contact)
 app.get('/blog', blog)
 app.get('/blog-detail/:id', blogDetail)
+app.get('/add-blog', addBlog)
+app.post('/blog', handlePostBlog)
+app.get('/delete/:id', handleDeleteBlog)
+app.get('/edit-blog/:id', editBlog)
 
+const data = []
 
 function home(req, res) {
     res.render('index')
@@ -23,30 +29,45 @@ function contact(req, res) {
 }
 
 function blog(req, res) {
-    const data = [
-        {
-            id: 1,
-            title: "Data 1",
-            content: "Content 1"
-        },
-        {
-            id: 2,
-            title: "Data 2",
-            content: "Content 2"
-        },
-        {
-            id: 3,
-            title: "Data 3",
-            content: "Content 3"
-        }
-    ]
-    res.render('blog', { data })
+
+    res.render('blog', { data, title: "myblog" })
 }
 
 function blogDetail(req, res) {
     const { id } = req.params
 
-    res.render('blog-detail', { id })
+    const dataDetail = data[id]
+
+    res.render('blog-detail', { data: dataDetail })
+}
+
+function addBlog(req, res) {
+    const addBlogTitle = "Add Blog"
+    res.render("add-blog", { data: addBlogTitle })
+}
+
+function handlePostBlog(req, res) {
+    // const title = req.body.ujang
+    // const content = req.body.content
+
+    const { title, content, languages } = req.body
+
+    data.push({ title, content })
+    console.log(languages);
+
+    res.redirect('/blog')
+}
+
+function handleDeleteBlog(req, res) {
+    const { id } = req.params
+
+    data.splice(id, 1)
+    console.log("berhasil delete id", id);
+    res.redirect('/blog')
+}
+
+function editBlog(req, res) {
+    res.render("edit-blog")
 }
 
 app.listen(port, () => {
